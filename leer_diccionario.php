040a241f7ab386e42e6f409cb7f6365c0d42ecbb
NotAlba <alba2000.2014@gmail.com>
2019-10-10 19:23:33 +0200

    Lectura del diccionario

---
+++ leer_diccionario.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+</head>
+<body>
+	<?php
+
+	$lectura = file_get_contents('diccionari_paraules.txt');
+
+	$palabras= str_split($lectura, 5);
+	foreach($palabras as $val)
+	{
+	echo ($val); 
+	}
+	
+
+?>
+</body>
+</html>
