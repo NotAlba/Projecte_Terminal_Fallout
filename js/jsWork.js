add3443ff345f5953947b44ef580ac509052413d
super <rogertim3@gmail.com>
2019-10-11 16:31:02 +0200

    subida validatePass

---
+++ js/jsWork.js
@@ -0,0 +1,13 @@
+function validatePass(choosen_pass,correct_pass){
+	let contador = 0;
+	if(choosen_pass != correct_pass){
+			for (let i = 0; i < choosen_pass.length; i++) {
+				if(choosen_pass[i] == correct_pass[i]){
+					contador++;
+				}
+			}
+			console.log(contador);
+	}else{
+		console.log("has acertado la palabra");
+	}
+}
