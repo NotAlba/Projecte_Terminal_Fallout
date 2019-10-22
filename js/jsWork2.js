e5bedd427ed3acb619533426ea8c4671b5d2cc06
NotAlba <alba2000.2014@gmail.com>
2019-10-22 16:56:21 +0200

    Correccion animacion

--- js/jsWork2.js
+++ js/jsWork2.js
@@ -2,100 +2,113 @@
 let intentosGlobal= 4;
 console.log("intentos:" , intentosGlobal)
 function comprobar_pal(id,palabraCorrecta){
-	id.classList.add("disabled");
-	adicionPuntos = ".....";
-	let vida = "<span>[]</span>&nbsp";
-	let contador = 0;
-	let palabraSeleccionada = palabra = id.innerHTML;
-	let correct = palabraCorrecta.innerHTML;
-	correct = correct.replace("<br>","");
-	posicionBr = palabraSeleccionada.indexOf("<br>");
-	palabraSeleccionada = palabra.replace("<br>","");
-
-	if(palabraSeleccionada!= correct){
-		intentosGlobal--;
-		if(intentosGlobal!=0){
-			for (let i = 0; i < palabraSeleccionada.length; i++) {
-				if(palabraSeleccionada[i] == correct[i]){
-					contador++;
-				}
-			}
-			if(posicionBr == 1){
-				adicionPuntos = ".<br>...."
-			}else if (posicionBr == 2) {
-				adicionPuntos = "..<br>..."
-			}
-			else if (posicionBr == 3) {
-				adicionPuntos = "...<br>.."
-			}
-			else if (posicionBr == 4) {
-				adicionPuntos = "....<br>."
-			}
-
-			__rellenarMensajes(contador);
-			id.innerHTML = adicionPuntos;
-			id.style.pointerEvents = "none"
-			document.getElementById("intentos").innerHTML = intentosGlobal + " ATTEMPT(S) LEFT: " + vida.repeat(intentosGlobal);
-		}else{
-			__juegoPerdido();
-<<<<<<< HEAD
-			
-		}	
-=======
-			cronoFin();
-		}
->>>>>>> 398e93cadb716805d7b6a96a95f535a132e7c082
-	}
+id.classList.add("disabled");
+adicionPuntos = ".....";
+let vida = "<span>[]</span>&nbsp";
+let contador = 0;
+let palabraSeleccionada = palabra = id.innerHTML;
+let correct = palabraCorrecta.innerHTML;
+correct = correct.replace("<br>","");
+posicionBr = palabraSeleccionada.indexOf("<br>");
+palabraSeleccionada = palabra.replace("<br>","");
 
 
-	else{
-		__juegoGanado();
-<<<<<<< HEAD
-		document.getElementById("vaultboywin").className += " finjuegoimagen";
 
-=======
-		__cronoFin();
-		__mensajePromptFinJuego();
->>>>>>> 398e93cadb716805d7b6a96a95f535a132e7c082
-	}
+if(palabraSeleccionada!= correct){
+intentosGlobal--;
+if(intentosGlobal!=0){
+for (let i = 0; i < palabraSeleccionada.length; i++) {
+if(palabraSeleccionada[i] == correct[i]){
+contador++;
+}
+}
+if(posicionBr == 1){
+adicionPuntos = ".<br>...."
+}
+else if (posicionBr == 2) {
+adicionPuntos = "..<br>..."
+}
+else if (posicionBr == 3) {
+adicionPuntos = "...<br>.."
+}
+else if (posicionBr == 4) {
+adicionPuntos = "....<br>."
 }
 
+__rellenarMensajes(contador);
+id.innerHTML = adicionPuntos;
+id.style.pointerEvents = "none"
+document.getElementById("intentos").innerHTML = intentosGlobal + " ATTEMPT(S) LEFT: " + vida.repeat(intentosGlobal);
+}else{
+__juegoPerdido();
+__cronoFin();
+}
+}
+
+
+else{
+__juegoGanado();
+__cronoFin();
+__mensajePromptFinJuego();
+document.getElementById("vaultboywin").className += " finjuegoimagen";
+}
+}
+
+function ayuda() {
+let numeroRandom = Math.floor(Math.random() * 2);
+let simbolosAyudaSelecionado = document.getElementById("ayuda");
+let puntosCarac = "." * simbolosAyudaSelecionado.length;
+simbolosAyudaSelecionado = puntosCarac;
+
+if (numeroRandom%2==0) {
+
+}
+else {
+intentosGlobal = 4;
+}
+
+}
+
+
 function __rellenarMensajes(contador){
-	let mensajesFallo = document.getElementsByClassName("mensajeFallo");
-	for (let i = mensajesFallo.length-1; i>=0;i--) {
-		if(i==0){
-			mensajesFallo[i].innerHTML = "Semejanza " + contador;
-		}else{
-			mensajesFallo[i].innerHTML = mensajesFallo[i-1].innerHTML
-		}
-	}
+let mensajesFallo = document.getElementsByClassName("mensajeFallo");
+for (let i = mensajesFallo.length-1; i>=0;i--) {
+if(i==0){
+mensajesFallo[i].innerHTML = "Semejanza " + contador;
+}else{
+mensajesFallo[i].innerHTML = mensajesFallo[i-1].innerHTML
+}
+}
 }
 
 function __juegoPerdido(){
-	document.getElementsByClassName("caracteres")[0].innerText = "has bloqueado el terminal!";
-	document.getElementById("intentos").innerHTML = "TERMINAL BLOQUEADA";
-	document.getElementsByClassName("codigo")[0].innerText = "";
-	document.getElementsByClassName("codigo")[1].innerText = "";
-	document.getElementById("vaultboyloss").className += " finjuegoimagen";
+document.getElementsByClassName("caracteres")[0].innerText = "has bloqueado el terminal!";
+document.getElementById("intentos").innerHTML = "TERMINAL BLOQUEADA";
+document.getElementsByClassName("codigo")[0].innerText = "";
+document.getElementsByClassName("codigo")[1].innerText = "";
+document.getElementById("tiempoCrono").innerHTML = "";
+document.getElementById("vaultboyloss").className += " finjuegoimagen";
 
-	for (let i = 0; i<mensajesFallo.length;i++) {
+for (let i = 0; i<mensajesFallo.length;i++) {
 
-		mensajesFallo[i].innerHTML = "";
-	}
+mensajesFallo[i].innerHTML = "";
+}
 }
 
 function __juegoGanado(){
-	let mensajesFallo = document.getElementsByClassName("mensajeFallo");
-	document.getElementsByClassName("caracteres")[0].innerText = "has desbloqueado el terminal!";
-	document.getElementById("intentos").innerHTML = "TERMINAL DESBLOQUEADA";
-	document.getElementsByClassName("codigo")[0].innerText = "";
-	document.getElementsByClassName("codigo")[1].innerText = "";
+let mensajesFallo = document.getElementsByClassName("mensajeFallo");
+document.getElementsByClassName("caracteres")[0].innerText = "has desbloqueado el terminal!";
+document.getElementById("intentos").innerHTML = "TERMINAL DESBLOQUEADA";
+document.getElementsByClassName("codigo")[0].innerText = "";
+document.getElementsByClassName("codigo")[1].innerText = "";
 
-	for (let i = 0; i<mensajesFallo.length;i++) {
+for (let i = 0; i<mensajesFallo.length;i++) {
 
-		mensajesFallo[i].innerHTML = "";
-	}
+mensajesFallo[i].innerHTML = "";
 }
+}
+
+
 
 
 // Parte del cronometro
@@ -105,36 +118,40 @@ let seg = 00;
 let min = 00;
 
 function crono() {
-	if (seg < 10) {
-		seg = "0" + seg;
-	}
-	if (seg == 60) {
-		seg = 00;
-		min++;
-		document.getElementById("minutos").innerHTML = min;
-	}
-	document.getElementById("segundos").innerHTML = seg;
-	seg++;
+if (seg < 10) {
+seg = "0" + seg;
+}
+if (seg == 60) {
+seg = "00";
+min++;
+document.getElementById("minutos").innerHTML = min;
+}
+document.getElementById("segundos").innerHTML = seg;
+seg++;
 }
 
 function cronoInicio() {
-	cronometro = setInterval(crono,1000);
+cronometro = setInterval(crono,1000);
 };
 
 function __cronoFin() {
-	clearInterval(cronometro);
+clearInterval(cronometro);
 };
 // Fin cronometro
 
 
 
 function __mensajePromptFinJuego() {
-	let nombreJugador = prompt("Escribe tu nombre");
-	if (seg < 10) {
-		seg = "0" + (seg-1);
-	}
-	document.getElementById("intentos").innerHTML = nombreJugador +" - "+ min+":"+seg;
+let nombreJugador = prompt("Escribe tu nombre");
+if (seg < 10) {
+seg = "0" + (seg-1);
 }
+document.getElementById("intentos").innerHTML = nombreJugador +" - Tiempo: "+ min+":"+seg +" - Vidas: "+ intentosGlobal;
+document.getElementById("tiempoCrono").innerHTML = "";
+}
+
+
+
 
 
 
