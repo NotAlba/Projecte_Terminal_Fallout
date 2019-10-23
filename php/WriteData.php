b34e153e0c3216f18aff1120c35671ff85c02e00
Roger <rogertim3@gmail.com>
2019-10-23 23:17:06 +0200

    subida metodo ordenar

--- php/WriteData.php
+++ php/WriteData.php
@@ -11,19 +11,37 @@
 
 		array_push($datosRanking,$jugador);
 
-		$json_string = json_encode($datosRanking,JSON_PRETTY_PRINT);
+		$datosOrdenados = array_orderby($datosRanking,'fallos',SORT_ASC,'tiempo',SORT_ASC, 'nombre',SORT_ASC);
+		$json_string = json_encode($datosOrdenados,JSON_PRETTY_PRINT);
 		$file = '../resources/dataPlayers.json';
 		file_put_contents($file, $json_string);
 	}
 
 
 
-		function obtenerDatos(){
-			$data = file_get_contents("../resources/dataPlayers.json");
+	function obtenerDatos(){
+		$data = file_get_contents("../resources/dataPlayers.json");
 
-			if($data == ""){
-				return array();
-			}
-			return json_decode($data,true);
+		if($data == ""){
+			return array();
 		}
+		return json_decode($data,true);
+		}
+
+function array_orderby() {
+    $args = func_get_args();
+    $data = array_shift($args);
+    foreach ($args as $n => $field) {
+        if (is_string($field)) {
+            $tmp = array();
+            foreach ($data as $key => $row)
+                $tmp[$key] = $row[$field];
+            $args[$n] = $tmp;
+            }
+    }
+    $args[] = &$data;
+    call_user_func_array('array_multisort', $args);
+    return array_pop($args);
+	}
+
  ?>
